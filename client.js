a61dbd7c246393495d2365844d3b61966601d5e0
Paulo Santiago <p.santiago796@gmail.com>
2020-11-04 23:20:57 +0000

    added wasd movement in input, client now pushes object to input.js

--- client.js
+++ client.js
@@ -1,34 +1,23 @@
 const net = require('net');
-// Other server host & port id
-    // host: '135.23.222.131',
-    // port: 50542
+    // // My server host & port id
+    // host: '10.0.2.15',
+    // port: 50541
 //
 /**
  * Establishes connection with the game server
  */
 const connect = function() {
-  const conn = net.createConnection({ 
-    // My server host & port id
-    host: '10.0.2.15',
-    port: 50541
+  const conn = net.createConnection({
+    // Other server host & port id
+    host: '135.23.222.131',
+    port: 50542
+
   });
 
   conn.on('connect', () => {
     console.log("Successfully connected to game server");
     conn.write("Name: PS");
-    // conn.write("Move: up");
-    // setInterval(() => conn.write("Move: up"), 50);
-    // conn.write("Move: down");
-    // conn.write("Move: left");
-    // conn.write("Move: right");
   });
-
-  // Moves up when first connecting to server (could write in first conn.on statement)
-  // conn.on('connect', () => {
-  //   conn.write("Move: up");
-  //   conn.write("Move: up");
-
-  // });
 
   // interpret incoming data as text
   conn.setEncoding('utf8');
--- input.js
+++ input.js
@@ -1,8 +1,11 @@
+// Stores the active TCP connection object.
+let connection;
 /**
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -11,6 +14,22 @@ const setupInput = function() {
   const handleUserInput = (key) => {
     if (key === '\u0003') {
       process.exit();
+    }
+
+    if (key === 'w') {
+      connection.write("Move: up");
+    }
+
+    if (key === 'a') {
+      connection.write("Move: left");
+    }
+
+    if (key === 's') {
+      connection.write("Move: down");
+    }
+
+    if (key === 'd') {
+      connection.write("Move: right");
     }
     // process.stdout.write(key);
   };
