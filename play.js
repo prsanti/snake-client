3aa1021030360400b8447c9ffa96473b08e03dcb
Paulo Santiago <p.santiago796@gmail.com>
2020-11-04 20:36:02 +0000

    added a user input function, and closes program with ^C

--- play.js
+++ play.js
@@ -1,3 +1,30 @@
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  const handleUserInput = (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+    // process.stdout.write(key);
+  };
+
+  // need to REFERENCE the function, not call it like handleUserInput('data')
+  stdin.on('data', handleUserInput);
+
+  return stdin;
+
+};
+
+setupInput();
