6086794d5a913700992e078d6742b146da0ae8c2
Paulo Santiago <p.santiago796@gmail.com>
2020-11-04 20:42:18 +0000

    setupInput function in this file for user inputs

---
+++ input.js
@@ -0,0 +1,26 @@
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  const handleUserInput = (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+    // process.stdout.write(key);
+  };
+
+  // need to REFERENCE the function, not call it like handleUserInput('data')
+  stdin.on('data', handleUserInput);
+
+  return stdin;
+};
+
+module.exports = {
+  setupInput
+};
