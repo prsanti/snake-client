c10b89d8da46d30b9953a2c3bb2cf022279c49d3
Paulo Santiago <p.santiago796@gmail.com>
2020-11-05 00:56:31 +0000

    created constant variables for repeatable strings

---
+++ constants.js
@@ -0,0 +1,27 @@
+const IP = '135.23.222.131';
+const PORT = 50542;
+const MOVE = {
+  UP: "Move: up",
+  DOWN: "Move: down",
+  LEFT: "Move: left",
+  RIGHT: "Move: right"
+};
+
+const messages = {
+  heart: "Say: ❤️",
+  pog: "Say: POGGERS",
+  smile: "Say: :)"
+
+}
+
+module.exports = {
+  IP,
+  PORT,
+  MOVE,
+  messages
+};
+
+    // // My server host & port id
+    // host: '10.0.2.15',
+    // port: 50541
+//
